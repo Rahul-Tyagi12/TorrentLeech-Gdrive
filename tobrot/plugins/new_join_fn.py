3ee11af905864da44148c1a3aa23a9ea51a63959
Rahul-Tyagi12 <61530246+Rahul-Tyagi12@users.noreply.github.com>
2021-04-26 10:38:56 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -25,6 +25,6 @@ async def help_message_f(client, message):
     # display the /help
 
     await message.reply_text(
-        """join this group for help-- @GbotStoreSupport\n\n And also don't forget to star/fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""",
+        """༺LEECHER BOT༻ \n COMMANDS HELP : \n 1. /gleechwb - Reply to link to upload in gdrive \n 2./leechwb - Reply to link to upload in telegram \n 3. /ytdlwb - Reply to youtube link \n 4. /pytdlwb - Reply to a youtube playlist link \n 5. /renewme - To clear Storage of Bot \n 6. /tleechwb - For telegram leech \n 7. /cancelwb - Reply to link for cancel uploading \n 8. /statuswb - To check current status \n 9. /clearthumbnailwb - To clear thumbnails \n 10. /savethumbnailwb - To save thumbnail \n\n NOTE : ONLY REPLY TO LINKS WORKS DONT DO /command link""",
         disable_web_page_preview=True,
     )
